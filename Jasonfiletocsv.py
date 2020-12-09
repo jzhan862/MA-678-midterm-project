1a5d08c18526752cf6deb249b3818e737afff963
Jinzhe Zhang <69559094+jzhan862@users.noreply.github.com>
2020-12-09 10:36:02 +0800

    Add files via upload

---
+++ Jasonfiletocsv.py
@@ -0,0 +1,27 @@
+"""
+Created on Sat Dec  5 2020
+
+@author: Jinzhe Zhang
+"""
+
+import pandas as pd
+json_data = pd.read_json('yelp_academic_dataset_business.json',lines=True)
+json_data.head()
+bool = json_data['categories'].str.contains('Janpanese')
+filter_data = json_data[bool]
+filter_data.to_csv('yelp_academic_dataset_business.csv',index=False)
+
+json_data = pd.read_json('yelp_academic_dataset_tip.json',lines=True)
+json_data.head()
+filter_data.to_csv('yelp_academic_dataset_tip.csv',index=False)
+
+json_data = pd.read_json('yelp_academic_dataset_review.json',lines=True)
+json_data.head()
+filter_data.to_csv('yelp_academic_dataset_review.csv',index=False)
+
+json_data = pd.read_json('yelp_academic_dataset_review.json',lines=True)
+json_data.head()
+filter_data.to_csv('yelp_academic_dataset_review.csv',index=False)
+
+json_data = pd.read_json('yelp_academic_dataset_checkin.json',lines=True)
+filter_data.to_csv('yelp_academic_dataset_checkin.csv',index=False)
